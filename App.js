2d44f3c90aae845fffeda65a6a143cf199960b05
millken chen <millken chen>
2018-11-16 09:48:25 +0800

    update app

--- App.js
+++ App.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
-import Root from './app/root';
+import {
+  StatusBar,
+  StyleSheet,
+  Platform,
+  View
+} from 'react-native';
+import Container from './app/root';
+
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
+
+const CommonStatusBar = ({ backgroundColor, ...props }) => (
+  <View style={[styles.statusBar, { backgroundColor }]}>
+    <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+  </View>
+);
 
 export default class App extends React.Component {
-    render() {
-      return <Root />;
-    }
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: 'white' }}>
+        <CommonStatusBar backgroundColor="#005005" barStyle="light-content" />
+        <Container />
+      </View>
+    );
   }
+}
+
+const styles = StyleSheet.create({
+  statusBar: {
+    height: STATUSBAR_HEIGHT
+  }
+});
